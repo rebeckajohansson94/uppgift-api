a4358e93dff542f689de27abdadc2f8e726117f4
rebeckajohansson94 <rebeckaemmajohansson@gmail.com>
2025-03-27 13:22:58 +0100

    first push

---
+++ logic.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', () => {
+    addEventListeners(); 
+});
+
+function getUsers(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response =>{
+        if(!response.ok){
+            throw new Error('Could not fetch data: ' + response.status);
+        }
+        return response.json();
+    })
+    .then(users =>{
+        document.querySelector('.user-cards').innerHTML = "";
+
+        users.forEach(user => {
+            let userContent = `
+                <div class="user-card" data-id="${user.id}">
+                <img class="user-img" src="../img/user-img.jpeg" alt="picture of user-icon"/>
+                <h3>Name: ${user.name}</h3>
+                <h4>Username: ${user.username}</h4>
+                <p><strong>Email: ${user.email}</strong></p>  
+                <button class="btn-show">Show more info</button>        
+                
+                <div class="more-info">
+                <p><strong>City:</strong> ${user.address.city}</p>
+                <p><strong>Phone:</strong> ${user.phone}</p>
+                <p><strong>Company:</strong> ${user.company.name}</p>
+                <button class="btn-close">Close</button>  
+                
+
+                </div>
+                </div>
+                `;
+            document.querySelector('.user-cards').innerHTML += userContent;
+            
+        });
+        
+    })
+
+    .catch(e =>{
+        alert('Error' + e);
+    });
+}
+
+getUsers();
+
